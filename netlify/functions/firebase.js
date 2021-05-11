5588286156da5bf4ac02f18ed2bd92d6b0d048cd
NickKelloggDDD <nicholas.mcdermott@kellogg.northwestern.edu>
2021-05-11 10:23:55 -0500

    commit 1

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,16 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = { } // replace
+const firebaseConfig = {
+ 
+    apiKey: "AIzaSyCQhmxE-JDycCE9USqhBTHKICQSg7rGTLs",
+    authDomain: "kiei-451-60e69.firebaseapp.com",
+    projectId: "kiei-451-60e69",
+    storageBucket: "kiei-451-60e69.appspot.com",
+    messagingSenderId: "45804692184",
+    appId: "1:45804692184:web:f368d3f0639458c0a5edad"
+
+ } // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
--- netlify/functions/posts.js
+++ netlify/functions/posts.js
@@ -21,26 +21,57 @@ let firebase = require(`./firebase`)
 // /.netlify/functions/posts
 exports.handler = async function(event) {
   // define an empty Array to hold the return value from our lambda
-
+  let returnValue = []
+  
   // establish a connection to firebase in memory
+  let db = firebase.firestore()
 
   // perform a query against firestore for all posts, wait for it to return, store in memory
+  let postsQuery = await db.collection(`posts`).get()
 
   // retrieve the documents from the query
+  let posts =postsQuery.docs
+
+  // //write the contents of the posts to the back-end console
+  // console.log(posts)
 
   // loop through the post documents
+  for (let postIndex=0; postIndex < posts.length; postIndex++){
     // get the id from the document
+    let postId = posts[postIndex].id
+    console.log(postId)
+
     // get the data from the document
+    let postData = posts[postIndex].data()
+    console.log(postData)
+
     // create an Object to be added to the return value of our lambda
+      let postObject = {
+        id: postId,
+        imageUrl: postData.imageUrl,
+        numberOfLikes: postData.numberOfLikes,
+        comments: []
+      }
+
     // get the comments for this post, wait for it to return, store in memory
+    let commentsQuery = await db.collection(`comments`).where(`postId`, `==`, postId).get()
+
     // get the documents from the query
+    let comments = commentsQuery.docs
+
     // loop through the comment documents
+    for(let commentIndex=0; commentIndex<comments.length; commentIndex++)  
+
       // get the id from the comment document
+      let commentId = comments[commentIndex].id
+
       // get the data from the comment document
       // create an Object to be added to the comments Array of the post
       // add the Object to the post
     // add the Object to the return value
-
+    returnValue.push(postObject)
+  }
+  
   // return value of our lambda
   return {
     statusCode: 200,
